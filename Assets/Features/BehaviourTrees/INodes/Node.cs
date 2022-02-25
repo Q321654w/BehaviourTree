511d3cdc230895abc40e5a017735e9d8f8ccd25d
Q321654w <80633262+Q321654w@users.noreply.github.com>
2022-02-25 21:25:22 +0200

    NodeWithStatusState

---
+++ Assets/Features/BehaviourTrees/INodes/Node.cs
@@ -0,0 +1,24 @@
+﻿namespace Features.BehaviourTrees.INodes.Implementations.Actions
+{
+    public abstract class Node : INode
+    {
+        protected Status Status;
+
+        public Node()
+        {
+            Status = Status.Idle;
+        }
+
+        public Status ExecutionStatus()
+        {
+            return Status;
+        }
+
+        public abstract void Enter();
+
+        public abstract void Execute();
+
+        public abstract void Exit();
+
+    }
+}
