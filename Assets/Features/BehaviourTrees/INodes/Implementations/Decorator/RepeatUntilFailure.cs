3bbe41d1ec456b3276e32ff20a36c26f3a43bd6c
Q321654w <80633262+Q321654w@users.noreply.github.com>
2022-03-11 21:29:14 +0200

    Refactoring

---
+++ Assets/Features/BehaviourTrees/INodes/Implementations/Decorator/RepeatUntilFailure.cs
@@ -0,0 +1,41 @@
+﻿namespace Features.BehaviourTrees.INodes.Implementations.Decorator
+{
+    public class RepeatUntilFailure : NodeDecorator
+    {
+        public RepeatUntilFailure(INode childNode) : base(childNode)
+        {
+        }
+
+        public override Status ExecutionStatus()
+        {
+            if (ChildNode.ExecutionStatus() != Status.Failure)
+                return Status.Running;
+
+            return ChildNode.ExecutionStatus();
+        }
+
+        public override void Enter()
+        {
+            ChildNode.Enter();
+            
+        }
+
+        public override void Execute()
+        {
+            var childStatus = ChildNode.ExecutionStatus();
+            if (childStatus != Status.Running && childStatus != Status.Idle)
+            {
+               
+                ChildNode.Exit();
+                ChildNode.Enter();
+            }
+
+            ChildNode.Execute();
+        }
+
+        public override void Exit()
+        {
+            ChildNode.Exit();
+        }
+    }
+}
